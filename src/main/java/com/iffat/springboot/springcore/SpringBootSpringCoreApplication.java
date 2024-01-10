ab7b6242150119987c7499ae18ae6e15e91dd4d7
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-10 07:52:13 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springboot/springcore/SpringBootSpringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springboot.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSpringCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iffat/springboot/springcore/SpringBootSpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.springboot.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpringCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
